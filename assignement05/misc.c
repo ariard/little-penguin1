de1c9c60113d64e11a68615130b9d304bda6bf2e
Antoine Riard <ariard@student.42.fr>
2018-03-25 18:39:17 +0200

    [assignement05] done

--- assignement05/misc.c
+++ assignement05/misc.c
@@ -3,6 +3,9 @@
 #include <linux/init.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/string.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Antoine Riard <ariard@student.42.fr>");
@@ -11,13 +14,36 @@ MODULE_DESCRIPTION("misc char device driver module");
 static ssize_t misc_read(struct file *filp, char __user *buffer,
 			size_t length, loff_t *offset)
 {
-	;
+	ssize_t		retval = 0;
+	ssize_t		r;
+
+	if ((r = copy_to_user(buffer, "ariard", 6)))
+		retval = -EFAULT;
+	else
+		retval = r;
+
+	return retval;
 }
 
 static ssize_t misc_write(struct file *filp, const char __user *buffer,
 			size_t length, loff_t *offset)
 {
-	;
+	ssize_t		retval = 0;
+	char		wr_buf[6];
+	
+	memset(wr_buf, 0, 6);
+	if (copy_from_user(wr_buf, buffer, 6)) {
+		retval = -EFAULT;
+		goto out;
+	}
+	
+	if (!(memcmp(wr_buf, "ariard", 6)))
+		retval = 6;
+	else
+		retval = -EINVAL;
+
+out:
+	return retval;
 }
 
 static struct file_operations misc_fops = {
