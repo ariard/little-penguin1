05fadcad6895ade4d6f37b4ad58f61d1686464df
Antoine Riard <ariard@student.42.fr>
2018-04-21 17:48:50 +0200

    [ass07] right

--- assignement07/debugfs.c
+++ assignement07/debugfs.c
@@ -16,7 +16,6 @@ MODULE_DESCRIPTION("Debugfs test module");
 
 static struct dentry *debugfs_dir = NULL;
 
-static u64 j = 0;
 
 static char *foo_page = NULL;
 
@@ -62,6 +61,34 @@ out:
 	return retval;
 }
 
+
+static ssize_t jiffies_read(struct file *filp, char __user *buffer,
+		size_t length, loff_t *offset)
+{
+	ssize_t		retval = 0;
+	u64 		j = 0;
+	char		s[30];
+	size_t		count;
+	size_t		size;
+
+	memset(s, 0, 30);
+	j = get_jiffies_64();
+	snprintf(s, 30, "%lld", j);
+
+	size = strlen(s);
+	count = (length < size) ? length : size;
+	if ((copy_to_user(buffer, s, size)))
+		retval = -EFAULT;
+	else
+		retval = size;
+
+	return retval;
+}
+
+static struct file_operations jiffies_fops = {
+	.read = jiffies_read,
+};
+
 static struct file_operations id_fops = {
 	.read = id_read,
 	.write = id_write,
@@ -77,7 +104,7 @@ static ssize_t foo_read(struct file *filp, char __user *buffer,
 
 	down_read(&sem);
 	size = strlen(foo_page);
-	count = (size > length) ? length : size;
+	count = (length < size) ? length : size;
 	if ((r = copy_to_user(buffer, foo_page, count)))
 		retval = -EFAULT;
 	else
@@ -95,7 +122,7 @@ static ssize_t foo_write(struct file *filp, const char __user *buffer,
 
 	down_write(&sem);
 	if (!foo_page) {
-		foo_page = kmalloc(sizeof(char) * PAGE_SIZE, GFP_KERNEL);
+		foo_page = kmalloc(PAGE_SIZE + 1, GFP_KERNEL);
 		memset(foo_page, 0, PAGE_SIZE);
 	}
 
@@ -106,6 +133,7 @@ static ssize_t foo_write(struct file *filp, const char __user *buffer,
 	else
 		memset(foo_page, 0, PAGE_SIZE);
 
+	count = (length < PAGE_SIZE) ? length : PAGE_SIZE;
 	if (copy_from_user(foo_page, buffer, count))
 		retval = -EFAULT;
 	else
@@ -136,8 +164,7 @@ static int __init debugfs_init(void)
 	if (!debugfs_create_file("id", 0666, debugfs_dir, NULL, &id_fops))
 		printk((KERN_ERR"debug driver : id file debug creation failed\n"));
 
-	j = get_jiffies_64();
-	if (!debugfs_create_u64("jiffies", 0444, debugfs_dir, &j))
+	if (!debugfs_create_file("jiffies", 0444, debugfs_dir, NULL, &jiffies_fops))
 		printk((KERN_ERR"debug driver : jiffies file debug creation failed\n"));
 
 	if (!debugfs_create_file("foo", 0644, debugfs_dir, NULL, &foo_ops))
