dc3480f3a2c3e9ef2432ff4af0ea0ff9cd3f9bee
Antoine Riard <ariard@student.42.fr>
2018-04-23 21:03:28 +0200

    [ass09] full path father

--- assignement09/main.c
+++ assignement09/main.c
@@ -21,34 +21,44 @@ int		mymounts_open(struct inode *inode, struct file *filp)
 	ssize_t			retval = 0;
 	struct mnt_namespace	*mnt = NULL;
 	struct mount		*tmp;
+	struct mount		*father = NULL;
 	size_t			size = 0;
 	char			raw[256];
-	char			b[512];
+	char			b[600];
 	char			*path;
 	char			fullpath[512];
+	int			prev_id;
+	int			i;
 
 	if (!(buf = kmalloc(PAGE_SIZE, GFP_KERNEL))) {
 	 	retval = -ENOMEM;
 		goto err;
 	}
-	
+
 	mnt = current->nsproxy->mnt_ns;
 	down_read(&namespace_sem);
 	list_for_each_entry(tmp, &mnt->list, mnt_list) {
 		if (!tmp->mnt_mp)
 			continue;
-		memset(fullpath, 0, 256);
-		memset(raw, 0, 256);
-		path = dentry_path_raw(tmp->mnt_parent->mnt_mountpoint, raw, 256);
-		strcat(fullpath, path);
+		memset(fullpath, 0, 512);
 		memset(raw, 0, 256);
 		path = dentry_path_raw(tmp->mnt_mountpoint, raw, 256);
-		strcat(fullpath, path);
-		memset(b, 0, 512);
-		if (strncmp(fullpath, "//", 2) == 0)
-			sprintf(b, "%-20s %s\n", tmp->mnt_devname, &fullpath[1]);
-		else	
-			sprintf(b, "%-20s %s\n", tmp->mnt_devname, fullpath);
+		i = 512 - strlen(path) - 1;
+		strcat(&fullpath[i], path);
+		father = tmp->mnt_parent;
+		while (father && father->mnt_mountpoint && prev_id != father->mnt_id) {
+			memset(raw, 0, 256);
+			path = dentry_path_raw(father->mnt_mountpoint, raw, 256);
+			if ((i = i - strlen(path)) < 0)
+				break;
+			memcpy(&fullpath[i], path, strlen(path));
+			prev_id = father->mnt_id;
+			father = father->mnt_parent;
+		}
+		memset(b, 0, 600);
+		while (fullpath[i + 1] && fullpath[i + 1] == '/')
+			i++;
+		sprintf(b, "%-20s %s\n", tmp->mnt_devname, &fullpath[i]);
 		size += strlen(b);
 		if (size > PAGE_SIZE)
 			break;
